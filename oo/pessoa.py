b22c6adf2379efe0055d769d0d4849b718a7122a
Henrique <henitz@gmail.com>
2020-04-02 12:05:01 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+     olhos = 2
      def __init__(self, *filhos, nome=None, idade=35):
           self.idade = idade
           self.nome = nome
           self.filhos = list(filhos)
      def Cumprimentar(self):
           return f'Ola {id(self)}'
+
+     @staticmethod
+     def metod_estatico():
+          return 42
+     @classmethod
+     def nome_e_atributos_de_classe(cls):
+          return f' (cls) - olhos {cls.olhos}'
 
 if __name__ == '__main__':
      renzo  = Pessoa(nome='Renzo')
@@ -18,6 +26,19 @@ if __name__ == '__main__':
           print(filho.nome)
      luciano.sobrenome = 'Ramalho'
      del luciano.filhos
+
+
      print(luciano.__dict__)
      print(renzo.__dict__)
+     print(Pessoa.olhos)
+     print (luciano.olhos)
+     print (renzo.olhos)
+
+
+
+
+     print(id(Pessoa.olhos), id(luciano.olhos),id(renzo.olhos))
+
+     print(Pessoa.metod_estatico(), luciano.metod_estatico())
+   #  print(Pessoa.nome_e_atributos_de_classe(),Luciano.metodo_estatico())
 
